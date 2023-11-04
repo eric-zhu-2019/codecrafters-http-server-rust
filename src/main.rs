cf5c2603fa919b29417b791116982f542a98c8b1
Eric Zhu <ezhu5121@outlook.com>
2023-11-04 11:08:40 -0700

    refactor request handling

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,43 @@
-use std::io::Write;
+use std::io::{BufRead, Write};
 // Uncomment this block to pass the first stage
+use anyhow::Result;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use anyhow::Result;
-use tokio::io::AsyncBufReadExt;
 
-async fn handle_request(mut stream: TcpStream) -> Result<()> {
-    let stream1 = tokio::net::TcpStream::from_std(stream.try_clone()?)?;
-    let reader = tokio::io::BufReader::new(stream1);
-    let mut lines = reader.lines();
-    if let Some(start_line) = lines.next_line().await? {
-        let mut iter = start_line.split_whitespace();
-        if let Some("GET") = iter.next()  {
-             if let Some(path) = iter.next() {
-                 if path == "/" {
-                     let ok = b"HTTP/1.1 200 OK\r\n\r\n";
-                     stream.write(ok)?;
-                 } else {
-                    let not_found = b"HTTP/1.1 404 NOT FOUND\r\n\r\n";
-                    stream.write(not_found)?;
-                 }
-                 stream.flush()?;
-             }
+const NOTFOUND: &'static str = "HTTP/1.1 404 Not Found\r\n";
+const OK: &'static str = "HTTP/1.1 200 Ok";
+
+fn handle_request(mut stream: TcpStream) -> Result<()> {
+    //let stream1 = tokio::net::TcpStream::from_std(stream.try_clone()?)?;
+    let mut reader = std::io::BufReader::new(&stream);
+    let mut start_line = String::new();
+    reader.read_line(&mut start_line)?;
+    let mut iter = start_line.split_whitespace();
+    if let Some("GET") = iter.next() {
+        let path = iter.next().unwrap();
+        if path == "/" {
+            stream.write(OK.as_bytes())?;
+            stream.flush()?;
+            return Ok(());
         }
     }
+    stream.write(NOTFOUND.as_bytes())?;
+    stream.flush()?;
+
     Ok(())
 }
 
-#[tokio::main]
-async fn main() -> Result<()>{
+fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 println!("accepted new connection");
-                handle_request(stream).await?;
+                handle_request(stream)?;
             }
             Err(e) => {
                 println!("error: {}", e);
