65bfffa7a25ad908f1b37b6d15e225811028cc5d
Eric Zhu <ezhu5121@outlook.com>
2023-11-04 19:37:28 -0700

    respond with content

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,44 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 
 const NOTFOUND: &'static str = "HTTP/1.1 404 Not Found\r\n";
-const OK: &'static str = "HTTP/1.1 200 Ok";
+const OK: &'static str = "HTTP/1.1 200 Ok\r\n";
 
+fn respond_error(mut stream: &TcpStream) {
+    stream.write(NOTFOUND.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
 fn handle_request(mut stream: TcpStream) -> Result<()> {
-    //let stream1 = tokio::net::TcpStream::from_std(stream.try_clone()?)?;
     let mut reader = std::io::BufReader::new(&stream);
     let mut start_line = String::new();
     reader.read_line(&mut start_line)?;
+
     let mut iter = start_line.split_whitespace();
     if let Some("GET") = iter.next() {
         let path = iter.next().unwrap();
-        if path == "/" {
-            stream.write(OK.as_bytes())?;
-            stream.flush()?;
-            return Ok(());
+        let paths = path.split("/").skip(1).collect::<Vec<&str>>();
+
+        match paths[0] {
+            "" => {
+                stream.write(OK.as_bytes())?;
+                stream.flush()?;
+                return Ok(());
+            }
+            "echo" => {
+                if paths.len() < 2 {
+                    respond_error(&stream);
+                    return Ok(());
+                }
+                stream.write(OK.as_bytes())?;
+                let hdr = format!(
+                    "Content-Type: text/plain\r\nContent-Length: {}\r\n\r\n",
+                    paths[1].len()
+                );
+                stream.write(hdr.as_bytes())?;
+                stream.write(paths[1].as_bytes())?;
+                stream.flush()?;
+                return Ok(());
+            }
+            _ => {}
         }
     }
     stream.write(NOTFOUND.as_bytes())?;
